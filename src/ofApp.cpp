0daa6c96dd27cfd0407c1dd077b3a0633ba16e71
Jonathan Dahan <jonathan@jonathan.is>
2015-03-20 13:33:09 -0400

    add kinect

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,19 +3,47 @@
 #define MS_FOR_SELECTION 3000
 
 void ofApp::setup(){
+  ofEnableSmoothing();
+  ofBackground(0);
+  ofGetWindowPtr()->setWindowTitle("fireworks");
+  startTime = endTime = -1;
+ 
+  setupCircles();
+  setupKinect();
+}
+
+void ofApp::setupCircles() {
   int w = ofGetViewportWidth();
   int h = ofGetViewportHeight();
-  ofEnableSmoothing();
   ofSetCircleResolution(180);
   circles.push_back(Circle(ofColor(0,255,255), 2 * w/6, h/2, 100));
   circles.push_back(Circle(ofColor(255,0,255), 3 * w/6, h/2, 100));
   circles.push_back(Circle(ofColor(255,255,0), 4 * w/6, h/2, 100));
   selectedColor = circles[0].c;
-  ofBackground(0);
-  ofGetWindowPtr()->setWindowTitle("fireworks");
-  startTime = endTime = -1;
   line.setStrokeWidth(1);
   line.setCircleResolution(180);
+}
+
+void ofApp::setupKinect() {
+	// enable depth->video image calibration
+	kinect.setRegistration(true);
+	kinect.init(false, false); // disable video image (faster fps)
+	kinect.open();		// opens first available kinect
+	
+	colorImg.allocate(kinect.width, kinect.height);
+	grayImage.allocate(kinect.width, kinect.height);
+	grayThreshNear.allocate(kinect.width, kinect.height);
+	grayThreshFar.allocate(kinect.width, kinect.height);
+	
+	nearThreshold = 230;
+	farThreshold = 200;
+	bThreshWithOpenCV = true;
+	
+	ofSetFrameRate(60);
+	
+	// zero the tilt on startup
+	angle = 0;
+	kinect.setCameraTiltAngle(angle);
 }
 
 void ofApp::update(){
@@ -56,10 +84,6 @@ void ofApp::draw(){
   for(ofMesh firework : fireworks){
     firework.drawVertices();
   }
-}
-
-void ofApp::keyPressed(int key){
-
 }
 
 void ofApp::keyReleased(int key){
@@ -128,4 +152,173 @@ void ofApp::gotMessage(ofMessage msg) {
 
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
+}
+
+
+void ofApp::updateKinect() {
+	
+	ofBackground(100, 100, 100);
+	
+	kinect.update();
+	
+	// there is a new frame and we are connected
+	if(kinect.isFrameNew()) {
+		
+		// load grayscale depth image from the kinect source
+		grayImage.setFromPixels(kinect.getDepthPixels());
+		
+		// we do two thresholds - one for the far plane and one for the near plane
+		// we then do a cvAnd to get the pixels which are a union of the two thresholds
+		if(bThreshWithOpenCV) {
+			grayThreshNear = grayImage;
+			grayThreshFar = grayImage;
+			grayThreshNear.threshold(nearThreshold, true);
+			grayThreshFar.threshold(farThreshold);
+			cvAnd(grayThreshNear.getCvImage(), grayThreshFar.getCvImage(), grayImage.getCvImage(), NULL);
+		} else {
+			
+			// or we do it ourselves - show people how they can work with the pixels
+			ofPixels & pix = grayImage.getPixels();
+			int numPixels = pix.size();
+			for(int i = 0; i < numPixels; i++) {
+				if(pix[i] < nearThreshold && pix[i] > farThreshold) {
+					pix[i] = 255;
+				} else {
+					pix[i] = 0;
+				}
+			}
+		}
+		
+		// update the cv images
+		grayImage.flagImageChanged();
+		
+		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
+		// also, find holes is set to true so we will get interior contours as well....
+		contourFinder.findContours(grayImage, 10, (kinect.width*kinect.height)/2, 20, false);
+	}
+	
+}
+
+void ofApp::drawKinect() {
+	
+	ofSetColor(255, 255, 255);
+	contourFinder.draw(10, 320, 400, 300);
+	
+	// draw instructions
+	ofSetColor(255, 255, 255);
+	stringstream reportStream;
+        
+    if(kinect.hasAccelControl()) {
+        reportStream << "accel is: " << ofToString(kinect.getMksAccel().x, 2) << " / "
+        << ofToString(kinect.getMksAccel().y, 2) << " / "
+        << ofToString(kinect.getMksAccel().z, 2) << endl;
+    } else {
+        reportStream << "Note: this is a newer Xbox Kinect or Kinect For Windows device," << endl
+		<< "motor / led / accel controls are not currently supported" << endl << endl;
+    }
+    
+	reportStream << "press p to switch between images and point cloud, rotate the point cloud with the mouse" << endl
+	<< "using opencv threshold = " << bThreshWithOpenCV <<" (press spacebar)" << endl
+	<< "set near threshold " << nearThreshold << " (press: + -)" << endl
+	<< "set far threshold " << farThreshold << " (press: < >) num blobs found " << contourFinder.nBlobs
+	<< ", fps: " << ofGetFrameRate() << endl
+	<< "press c to close the connection and o to open it again, connection is: " << kinect.isConnected() << endl;
+
+    if(kinect.hasCamTiltControl()) {
+    	reportStream << "press UP and DOWN to change the tilt angle: " << angle << " degrees" << endl
+        << "press 1-5 & 0 to change the led mode" << endl;
+    }
+    
+	ofDrawBitmapString(reportStream.str(), 20, 652);
+    
+}
+
+void ofApp::exit() {
+	kinect.setCameraTiltAngle(0); // zero the tilt on exit
+	kinect.close();
+}
+
+void ofApp::keyPressed (int key) {
+	switch (key) {
+		case ' ':
+			bThreshWithOpenCV = !bThreshWithOpenCV;
+			break;
+			
+		case'p':
+			bDrawPointCloud = !bDrawPointCloud;
+			break;
+			
+		case '>':
+		case '.':
+			farThreshold ++;
+			if (farThreshold > 255) farThreshold = 255;
+			break;
+			
+		case '<':
+		case ',':
+			farThreshold --;
+			if (farThreshold < 0) farThreshold = 0;
+			break;
+			
+		case '+':
+		case '=':
+			nearThreshold ++;
+			if (nearThreshold > 255) nearThreshold = 255;
+			break;
+			
+		case '-':
+			nearThreshold --;
+			if (nearThreshold < 0) nearThreshold = 0;
+			break;
+			
+		case 'w':
+			kinect.enableDepthNearValueWhite(!kinect.isDepthNearValueWhite());
+			break;
+			
+		case 'o':
+			kinect.setCameraTiltAngle(angle); // go back to prev tilt
+			kinect.open();
+			break;
+			
+		case 'c':
+			kinect.setCameraTiltAngle(0); // zero the tilt
+			kinect.close();
+			break;
+			
+		case '1':
+			kinect.setLed(ofxKinect::LED_GREEN);
+			break;
+			
+		case '2':
+			kinect.setLed(ofxKinect::LED_YELLOW);
+			break;
+			
+		case '3':
+			kinect.setLed(ofxKinect::LED_RED);
+			break;
+			
+		case '4':
+			kinect.setLed(ofxKinect::LED_BLINK_GREEN);
+			break;
+			
+		case '5':
+			kinect.setLed(ofxKinect::LED_BLINK_YELLOW_RED);
+			break;
+			
+		case '0':
+			kinect.setLed(ofxKinect::LED_OFF);
+			break;
+			
+		case OF_KEY_UP:
+			angle++;
+			if(angle>30) angle=30;
+			kinect.setCameraTiltAngle(angle);
+			break;
+			
+		case OF_KEY_DOWN:
+			angle--;
+			if(angle<-30) angle=-30;
+			kinect.setCameraTiltAngle(angle);
+			break;
+	}
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -4,6 +4,9 @@
 #pragma once
 #include "Circle.h"
 
+#include "ofxKinect.h"
+#include "ofxOpenCv.h"
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -20,17 +23,39 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    void exit();
+
     void explode(int x, int y);
+    void setupKinect();
+    void drawKinect();
+    void updateKinect();
+    void setupCircles();
 		
     vector<Circle> circles;
     bool pointInCircle(int pX, int pY, Circle c);
     Circle * whichCircle();
     ofColor selectedColor;
-    ofShader shader, explodeshader;
     ofPath line;
     vector<ofMesh> fireworks;
 
     bool select;
     float startTime;
     float endTime;
+
+    ofxKinect kinect;
+    ofxCvColorImage colorImg;
+
+    ofxCvGrayscaleImage grayImage; // grayscale depth image
+    ofxCvGrayscaleImage grayThreshNear; // the near thresholded image
+    ofxCvGrayscaleImage grayThreshFar; // the far thresholded image
+
+    ofxCvContourFinder contourFinder;
+
+    bool bThreshWithOpenCV;
+    bool bDrawPointCloud;
+
+    int nearThreshold;
+    int farThreshold;
+
+    int angle;
 };
